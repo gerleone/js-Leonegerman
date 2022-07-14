fd132d9d42f260cbcf842188c026ec2d52f7895d
german leone <gerleone@hotmail.com>
2022-07-13 21:57:09 -0300

    arrays

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 
 
+const productos = [ { producto: 'zapatillas', id: 3322, stock: 2 },
+                    { producto: 'gorra', id: 3442, stock: 0 },
+                    { producto: 'remera', id: 5522, stock: 1 },
+                    { producto: 'pantalon', id: 6622, stock: 1 }
+]
+console.log(productos[1]);
+
+//---- agrega obejeto a la lista (push)
+productos.push({producto:'medias', id:9922, stock:3});
+
+// ---recorre la lista(length)
+for (let i= 0; i < productos.length; i++) {
+    console.log(productos[i]);
+}
+
+// ----elimina un objeto de la lista(splice)
+productos.splice(4, 1)
+
+console.log(productos)
 
 
-function agregarAlCarrito(producto, stock)  {
-    const tenemosStock = validarStock (stock) ;
-    if (tenemosStock === 'tenemos stock' )  {
-        console.log ('agregaste el producto al carrito: ' + producto)
-    } else{
-        console.log ('no tenemos stock')
+function agregarAlCarrito(producto, stock) {
+    const tenemosStock = validarStock(stock);
+    if (tenemosStock === 'tenemos stock') {
+        console.log('agregaste el producto al carrito: ' + producto)
+    } else {
+        console.log('no tenemos stock')
     }
 }
 
-function  validarStock (stock)  {
-    if (stock > 0)  {
-        return 'tenemos stock' ;
-    } else  {
-        return 'no tenemos stock' ;
+function validarStock(stock) {
+    if (stock > 0) {
+        return 'tenemos stock';
+    } else {
+        return 'no tenemos stock';
     }
 }
-
-agregarAlCarrito ('zapatilla', 1) 
-agregarAlCarrito ('Gorra', 0)
-agregarAlCarrito ('Pantalón', 1)
-agregarAlCarrito ('Remera', 1)
 
 const producto1 = 4000
 const producto2 = 500
@@ -32,4 +46,7 @@ function suma(numero1, numero2) {
     console.log(total);
 }
 suma(producto1, producto2)
+
+
+
 
